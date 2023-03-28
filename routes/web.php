a9c818546a266c597bfb29f384eb83a03919a8e9
FitranImanRamadhan <fitraniman11@gmail.com>
2023-03-28 17:20:06 +0700

    pertemuan 4

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController; //mendaftarkan controler yang akan digunakan
+use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,14 +15,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home', ['title' => 'Home']);
-})->name('home');
+
 
 Route::get('register', [UserController::class, 'register'])->name('register');
 Route::post('register', [UserController::class, 'register_action'])->name('register.action');
 Route::get('login', [UserController::class, 'login'])->name('login');
 Route::post('login', [UserController::class, 'login_action'])->name('login.action');
-Route::get('password', [UserController::class, 'password'])->name('password');
-Route::post('password', [UserController::class, 'password_action'])->name('password.action');
-Route::get('logout', [UserController::class, 'logout'])->name('logout');
+
+Route::middleware('auth')->group(
+    function () {
+        Route::get('/', function () {
+            return view('home', ['title' => 'Home']);
+        })->name('home');
+        Route::get('password', [UserController::class, 'password'])->name('password');
+        Route::post('password', [UserController::class, 'password_action'])->name('password.action');
+        Route::get('logout', [UserController::class, 'logout'])->name('logout');
+    }
+);
